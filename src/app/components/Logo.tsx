1728bac32fa36b1f0ad6617b99dd1dc162e4371e
Skrukka <wefling@live.no>
2024-11-12 19:43:59 +0100

    Cleanup

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -1,14 +1,10 @@
-import Image from "next/image"
-import omegalogo from "../public/omegalogo.png"
-import style from "./component.module.css"
+
 
 export function Logo() {
     const source = "https://omega.ntnu.no/static/8cb2cc79536a03aeaa04.png";
 
     return (
-
             <img src={source} alt="logo" />
-
     );
 
 }
