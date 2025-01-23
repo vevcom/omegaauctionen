db63d012cfaae27f513f2d5dc5bc104dfa28c4a1
Hsackboy <hakmikalsen@gmail.com>
2025-01-23 18:54:37 +0100

    Added basic auction adding page

---
+++ src/app/components/createAuctionItemFromForm.tsx
@@ -0,0 +1,45 @@
+"use server"
+
+import { prisma } from "../prisma";
+
+
+export async function createAuctionItemFromForm(e: FormData) {
+    if ((typeof (parseInt(e.get("startPriceInKroner") as string)) === "number") == false) {
+        return false;
+    }
+    if (((typeof (e.get("name")) === "string")) == false && e.get("name") != "") {
+        return false;
+    }
+    if (((typeof (e.get("descripton")) === "string")) == false && e.get("name") != "") {
+        return false;
+    }
+
+    await prisma.auksjonsObjekt.create({
+        data: {
+            currentSaleTime: new Date("2022-03-25"),
+            finalSaleTime: new Date("2022-03-25"),
+            description: e.get("descripton") as string,
+            name: e.get("name") as string,
+            startPriceOre: parseInt(e.get("startPriceInOre") as string) * 100,
+        }
+    })
+
+    return true;
+
+    // if (testAppproved == testNumber) {
+    //     await prisma.auksjonsObjekt.create({
+    //         data: {
+    //             currentSaleTime: new Date("2022-03-25"),
+    //             finalSaleTime: new Date("2022-03-25"),
+    //             description: e.get("descripton"),
+    //             name: e.get("name"),
+    //             startPriceOre: parseInt(e.get("startPris"))*100,
+    //         }
+    //     })
+    //     return 1
+    // }
+    // else{
+    //     return -1
+    // }
+
+}
