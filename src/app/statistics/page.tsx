61f26cf91f0ee10cd270b15139f6fb85717fafe2
Karoline <karolsga@stud.ntnu.no>
2025-01-23 20:15:24 +0100

    feat statistics

--- src/app/statistics/page.tsx
+++ src/app/statistics/page.tsx
@@ -1,14 +1,71 @@
-import style from "./page.module.scss"
+"use client"
+import 'chart.js/auto'
+import {Pie,Bar,Line,Doughnut,Radar,PolarArea,Bubble,Scatter} from "react-chartjs-2" //importerer ulike graf typer
+import { Chart } from 'chart.js';
+import styles from "./page.module.css";
+//import {ChartSankey} from 'chartjs-chart-sankey'; 
 
-export default function Statistics() {
+//Chart.register(ChartSankey);
+
+export default function Klassetrinn() {
+
+  const colors = [               //definert farger 
+    'rgb(240, 8, 58)',     //farge nummer 1 tilhører nå 1.klasse feks
+    "rgb(255,125,0)",
+    'rgb(252, 235, 5)',
+    'rgb(6, 202, 16)', 
+    'rgb(54, 162, 235)',     
+    'rgb(153, 102, 255)'
+  ];
 
 
-    return (<div>
-        <h2 className={style.titleCard}>Oi! Så mye fin statistikk!</h2>
-            <div style={{display:"flex",flexWrap:"wrap",width:"90vw",margin:"5vw"}}>
-                <div style={{backgroundColor:"var(--lightblue)",width:"35vw",height:"30vw",margin:"5vw"}}></div>
-                <div style={{backgroundColor:"var(--orange)",width:"35vw",height:"30vw",margin:"5vw"}}></div>
-                <div style={{backgroundColor:"var(--red)",width:"80vw",height:"10vw",marginLeft:"5vw"}}></div>
-            </div>
-        </div>)
+    const spenders = [
+      {
+        name: "1.klasse",    //navn
+        spent: 200,          //sum
+      },
+      {
+        name: "2.klasse",
+        spent: 240,
+      },      {
+        name: "3.klasse",
+        spent: 500,
+      },      { 
+        name: "4.klasse",
+        spent: 200,
+      },      {
+        name: "5.klasse",
+        spent: 100,
+      },
+            
+
+    ]
+
+    const data = { 
+      labels: spenders.map(a => a.name), 
+      datasets: [{
+        label: "dataset",
+        data: spenders.map(spenders => spenders.spent),
+        backgroundColor:colors,                          //farger på charts = farger definert i colors
+      }]
+    }
+
+
+    return (
+    <div style = {{width: "20%",margin: "0 auto",}}>
+        <h1 className={styles.overskrift}>Overskrift</h1> 
+        <Pie data={data}></Pie>    
+        <Bar className= {styles.bar} data={data}></Bar>
+        <Line data={data}></Line>  
+        <Doughnut data={data}></Doughnut>
+        <Radar data={data}></Radar>
+        <PolarArea data={data}></PolarArea>
+
+
+
+      </div>
+    )
 }
+
+
+
