e9f5e98691e948fb8109b2ba44f33e61f7c4ea4c
Hsackboy <hakmikalsen@gmail.com>
2025-01-31 14:43:50 +0100

    chore:fixed som alerts, added some documentaions and made it more user freindly

--- src/app/components/pictureServerComponents/getImgFormName.tsx
+++ src/app/components/pictureServerComponents/getImgFormName.tsx
@@ -1,10 +1,18 @@
 "use client";
 
-export default function ImageFromFileName({ filename }: { filename: string }) {
+export default function ImageFromFileName({ filename, styleComponent }: {
+    filename: string, styleComponent: {
+        readonly [key: string]: string;
+    }
+}) {
+    /*
+    Filname : filename of the picture you want to find. Get it from the database
+    Stylecomponent : for styling the element. add the style from the line "import style from "style.module.scss"
+    */
     const serverIP = 'http://127.0.0.1:5000'
     const imagePath = serverIP + '/image/'
     return (
 
-        <img className="auctionImage" src={imagePath + filename}></img>
+        <img className={`${styleComponent ? styleComponent.auctionImage : ""}`} src={imagePath + filename}></img>
     );
 }
--- src/app/put-item-for-auction/page.tsx
+++ src/app/put-item-for-auction/page.tsx
@@ -3,7 +3,6 @@ import React, { FormEvent, useState } from "react"
 import { createAuctionItemFromForm } from "../components/createAuctionItemFromForm"
 import ImageFromFileName from "../components/pictureServerComponents/getImgFormName"
 import ImageUploaderButton from "../components/pictureServerComponents/uploadButton"
-import { uploadImage } from "../components/pictureServerComponents/uploadImageServerFunction"
 import style from "./style.module.scss"
 import PopUpBox from "@/app/components/popUp/popUp"
 
@@ -13,23 +12,23 @@ export default function makeAuctionItem() {
     const [popUpText, SetPopUpText] = useState("")
     const popUpLengthMilliSeconds = 5000
 
-    const delay = ms => new Promise(
+    const delay = (ms:number) => new Promise(
         resolve => setTimeout(resolve, ms)
     );
 
 
     async function sendFormData(e: FormData) {
 
-        if (((typeof (parseInt(e.get("startPriceInKroner") as string)) === "number") == false) || ((e.get("startPriceInKroner") as string) == "")) {
-            alertBox("It looks like you havent typed in i valid number")
-            return;
-        }
         if ((((typeof (e.get("name")) === "string")) == false) || (e.get("name") == "")) {
-            alertBox("It looks like youre missing a name")
+            alertBox("Ser ut som du mangler et navn")
             return;
         }
         if ((((typeof (e.get("descripton")) === "string")) == false) || (e.get("descripton") == "")) {
-            alertBox("It looks like yyour missning a description")
+            alertBox("Ser ut som du mengler en beskrivelse")
+            return;
+        }
+        if (((typeof (parseInt(e.get("startPriceInKroner") as string)) === "number") == false) || ((e.get("startPriceInKroner") as string) == "")|| parseInt(e.get("startPriceInKroner") as string)<0) {
+            alertBox("Ser ut som du ikke skrev inn et gylding nummmer")
             return;
         }
         e.append("imageFileName", uploadedFileName)
@@ -38,10 +37,10 @@ export default function makeAuctionItem() {
 
 
         if (response == true) {
-            alertBox("It worked")
+            alertBox("Det funket :)")
         }
         else {
-            alertBox("It did'nt work")
+            alertBox("Det funket ikke. vent litt og prøv igjen")
         }
     }
 
@@ -76,15 +75,9 @@ export default function makeAuctionItem() {
             </form>
             <h1>last opp bilde</h1>
             <ImageUploaderButton
-                uploadImage={uploadImage}
                 setUploadedFileName={setUploadedFileName}
             />
-            {uploadedFileName && (
-                <div className="mt-4 text-green-500">
-                    Uploaded File: {uploadedFileName}
-                </div>
-            )}
-            <ImageFromFileName filename={uploadedFileName}></ImageFromFileName>
+            <ImageFromFileName styleComponent={style} filename={uploadedFileName}></ImageFromFileName>
         </div >
     )
 }
