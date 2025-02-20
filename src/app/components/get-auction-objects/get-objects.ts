f1d454dd6fb0b598ab0d1c9de1c7aa0c3fe7835d
Hsackboy <hakmikalsen@gmail.com>
2025-02-20 18:12:11 +0100

    feat: Get all object function and sample code

--- src/app/components/get-auction-objects/get-objects.ts
+++ src/app/components/get-auction-objects/get-objects.ts
@@ -4,23 +4,28 @@ import { prisma } from "@/app/prisma"
 import sortObjectsFunc from "@/app/components/get-auction-objects/sort-objects-func"
 
 
-export default async function get_objects_all(sortType:string,reverse=false) {
+export default async function get_objects_all(sortType: string, reverse = false, adminMode = false) {
     const itemsPerPage = parseInt(process.env.NEXT_PUBLIC_OBJECTS_PER_PAGE as string)
-    let objekter = await prisma.auksjonsObjekt.findMany()
+    let objekter = await prisma.auksjonsObjekt.findMany({
+        where:
+        {
+            approved: !adminMode,
+        },
+    })
     let objectAmount = objekter.length
     let pagesList = []
-    
-    objekter = await sortObjectsFunc(objekter,sortType,reverse)
 
-    let currentPageList:any = []
-    for(let i = 0; i <objectAmount;i++){
-        if (currentPageList.length == itemsPerPage){
+    objekter = await sortObjectsFunc(objekter, sortType, reverse)
+
+    let currentPageList: any = []
+    for (let i = 0; i < objectAmount; i++) {
+        if (currentPageList.length == itemsPerPage) {
             pagesList.push(currentPageList)
             currentPageList = []
         }
         currentPageList.push(objekter.at(i))
     }
-    if (currentPageList.length != 0){
+    if (currentPageList.length != 0) {
         pagesList.push(currentPageList)
     }
     return pagesList
