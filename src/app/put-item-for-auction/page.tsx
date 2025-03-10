f81173f46cc517af1d0c79663ccda1e27862f559
Hsackboy <hakmikalsen@gmail.com>
2025-03-10 16:08:29 +0100

    Fix:samll fix

--- src/app/put-item-for-auction/page.tsx
+++ src/app/put-item-for-auction/page.tsx
@@ -82,7 +82,7 @@ export default function makeAuctionItem() {
                 </div>
             </form>
             <div className={style.uploaderDiv}>
-                <h1>Last opp egendefinert bilde.</h1>
+                <h1>Last opp egendefinert bilde. <b>Merk!</b> Noen ganger kommer ikke bilde frem. Hvis bilde ikke vises etter noen sekunder, så kan du prøve å trykke last opp på nytt</h1>
                 <ImageUploaderButton styleNameBrowse={style.borwseButton} styleNameButton={style.uploaderButton} setUploadedFileName={setUploadedFileName} />
                 <ImageFromFileName style={style.preveiwImage} filename={uploadedFileName}></ImageFromFileName>
             </div>
