9a73a332de943adca5ec92b769c76c48dec75d4f
Hsackboy <hakmikalsen@gmail.com>
2025-03-12 14:46:33 +0100

    feat: right buttons for the item pages

--- src/app/live-auction/page.tsx
+++ src/app/live-auction/page.tsx
@@ -22,23 +22,27 @@ export default function App() {
         <div>
             <h1 className={style.headLine}>Kjøp live!</h1>
             <h3 className={style.promoText}> Oi oi oi. Det selges ting live! Da kan du møte opp og by live! Akkuratt som storage wars!(Omega gir ingen garanti på liveauksjonens likhet til det amerikanske tv programmet storage wars). Møt opp EL5. Live salgene går i fire puljer.
-            
-            <br></br>
-            Pulje 1: __tid__
-            <br></br>
-            Pulje 2: __tid__
-            <br></br>
-            Pulje 3: __tid__
-            <br></br>
-            Pulje 4: __tid__
-            </h3> 
-            {/* <!-- TODO: add place --> */}
-            <p>{pageNumber + 1}/{allPages.length}</p>
-            <button onClick={() => setPageNumber((prev) => Math.max(0, prev - 1))}>Forrige</button>
-            <button onClick={() => setPageNumber((prev) => Math.min(allPages.length - 1, prev + 1))}>Neste</button>
+
+                <br></br>
+                Pulje 1: __tid__
+                <br></br>
+                Pulje 2: __tid__
+                <br></br>
+                Pulje 3: __tid__
+                <br></br>
+                Pulje 4: __tid__
+            </h3>
+            <div className={style.buttonContainer}>
+                <button className={style.knapp} onClick={() => setPageNumber((prev) => Math.max(0, prev - 1))}>Forrige</button>
+                <p className={style.sidetall}>{pageNumber + 1}/{allPages.length}</p>
+                <button className={style.knapp} onClick={() => setPageNumber((prev) => Math.min(allPages.length - 1, prev + 1))}>Neste</button>
+            </div>
             <ItemPageComponent pageTitle="Live auksjoner" allPages={allPages} currentPageNumber={pageNumber} />
-            <button onClick={() => setPageNumber((prev) => Math.max(0, prev - 1))}>Forrige</button>
-            <button onClick={() => setPageNumber((prev) => Math.min(allPages.length - 1, prev + 1))}>Neste</button>
+            <div className={style.buttonContainer}>
+                <button className={style.knapp} onClick={() => setPageNumber((prev) => Math.max(0, prev - 1))}>Forrige</button>
+                <p className={style.sidetall}>{pageNumber + 1}/{allPages.length}</p>
+                <button className={style.knapp} onClick={() => setPageNumber((prev) => Math.min(allPages.length - 1, prev + 1))}>Neste</button>
+            </div>
         </div>
     );
 }
