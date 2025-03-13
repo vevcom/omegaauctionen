37b670971c3de28a9712cb421659e5361217d851
Hsackboy <hakmikalsen@gmail.com>
2025-03-13 13:18:59 +0100

    feat: money made function

---
+++ src/app/components/get-money-made/get-money-made.ts
@@ -0,0 +1,92 @@
+"use server"
+
+import { prisma } from "@/app/prisma"
+import { AuksjonsObjektType } from "@prisma/client"
+
+
+// intentional extra split up for easier understanding and for copying parts of code for other potential statists later
+export default async function get_money_made() {
+    let sumInOre = 0 // initial value
+
+    //Money made from online auction
+    //gets sum of all current prices
+    const normalAuctionObjectsSumOre = await prisma.auksjonsObjekt.aggregate({
+        _sum: {
+            currentPriceOre: true
+        },
+        where: {
+            approved: true,
+            type: AuksjonsObjektType.AUKSJON
+        }
+    })
+
+    //checks if null
+    let moneyMadeOnlineAuctionOre = 0
+    if (!normalAuctionObjectsSumOre|| !normalAuctionObjectsSumOre._sum.currentPriceOre) {
+        moneyMadeOnlineAuctionOre = 0
+    }
+    else {
+        moneyMadeOnlineAuctionOre = normalAuctionObjectsSumOre._sum.currentPriceOre
+    }
+
+    //Money made from capes
+
+    //gets all capes objects(objects typed sale) selects count of bids and current price. So:
+    // simplified : [[price, bids] , [price, bids]]
+    const capesAuctionObjects = await prisma.auksjonsObjekt.findMany({
+        where: {
+            approved: true,
+            type: AuksjonsObjektType.SALG,
+        },
+        select: {
+            currentPriceOre: true,
+            _count: {
+                select: {
+                    bids: true
+                }
+            }
+        }
+    })
+
+    //checks if null
+    let moneyMadeCapeOre = 0
+    if (!capesAuctionObjects) {
+        moneyMadeCapeOre = 0
+    }
+    else {
+        //Product of price of cape and amount of bids
+        const moneyPerCapeTypeOre = capesAuctionObjects.map((object) => (object.currentPriceOre * object._count.bids))
+
+        //Sum of money made per cape
+        moneyMadeCapeOre = moneyPerCapeTypeOre.reduceRight((prev, current) => prev + current, 0);
+    }
+
+    // Money made from live registration objects. They are identified by containing "DONOTAPPROVE" in their name. The money made is stored kinda stupidly in the stock. but has its reasons
+    // gets sum of all monet made
+    const liveRegisteredAuctionObjectsSumOre = await prisma.auksjonsObjekt.aggregate({
+        _sum: {
+            stock: true
+        },
+        where: {
+            name: {
+                contains: "DONOTAPPORVE"
+            },
+            approved: false,
+        }
+    })
+
+    //checks if null
+    let moneyMadeLiveRegisteredAuctionObjectsOre
+    if (!liveRegisteredAuctionObjectsSumOre || !liveRegisteredAuctionObjectsSumOre._sum.stock) {
+        moneyMadeLiveRegisteredAuctionObjectsOre = 0
+    }
+    else {
+        moneyMadeLiveRegisteredAuctionObjectsOre = liveRegisteredAuctionObjectsSumOre._sum.stock
+    }
+
+    sumInOre += moneyMadeCapeOre
+    sumInOre += moneyMadeOnlineAuctionOre
+    sumInOre += moneyMadeLiveRegisteredAuctionObjectsOre
+
+    return sumInOre
+}
