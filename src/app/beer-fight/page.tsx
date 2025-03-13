11e13ee17156fdbe8de7dce350b1da720622406e
Hsackboy <hakmikalsen@gmail.com>
2025-03-13 18:14:12 +0100

    chore: changes

---
+++ src/app/beer-fight/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+import style from "./page.module.scss"
+
+
+
+
+export default function hansafight() {
+
+
+    return <div className={style.mainDiv}>
+        <h1 className={style.title}>Har du lyst på Hansa i kjøleskapet?</h1>
+        <h2 className={style.info}>Eller gjør det deg sint. Grr. Nå kan du bestemme! 1kr=1 stemme! Stem på det du vil. Den med flest stemmer vinner!</h2>
+
+    </div>
+}
