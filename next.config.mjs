1c4ccbbd808b188c8e0e1237c1ac62d82273dc14
Hsackboy <hakmikalsen@gmail.com>
2025-10-23 12:37:50 +0200

    fix: Add larger allowed picture size for upload

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,18 @@ const nextConfig = {
   typescript: {
     // WARNING, MUST BE REMOVED BEFORE PROD
     // FIXME,   MUST BE REMOVED BEFORE PROD
-    ignoreBuildErrors: true, 
+    ignoreBuildErrors: true,
   },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb',
+    }
+  }
 };
 
 export default nextConfig;
